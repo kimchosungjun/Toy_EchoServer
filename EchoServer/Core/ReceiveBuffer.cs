032e79df9d5fa5c9f06f2829ed41d1bdce93f2a5
Sung Jun Cho <chosung0531@naver.com>
2025-05-13 00:06:36 +0900

    Session (2)
    - Receive Buffer

--- EchoServer/Core/ReceiveBuffer.cs
+++ EchoServer/Core/ReceiveBuffer.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class ReceiveBuffer
+    public class ReceiveBuffer
     {
+        #region Variables
+        // Values
+        int readPosition;
+        int writePosition;
+        ArraySegment<byte> buffer;
+
+        // Property
+        public int ReadSize { get { return writePosition - readPosition; } }
+        public int WriteSize { get { return buffer.Count - writePosition; } }
+        public ArraySegment<byte> GetReadSegment { get { return new ArraySegment<byte>(buffer.Array, buffer.Offset + readPosition, ReadSize); } }
+        public ArraySegment<byte> GetWriteSegment { get { return new ArraySegment<byte>(buffer.Array, buffer.Offset + writePosition, WriteSize); } }
+        #endregion
+
+        #region Creator
+        /// <summary>
+        /// Default Size = 4KB
+        /// </summary>
+        /// <param name="_bufferSize"></param>
+        public ReceiveBuffer(int _bufferSize = 4096) 
+        {
+            buffer = new ArraySegment<byte>(new byte[_bufferSize], 0, _bufferSize);
+            readPosition = writePosition = 0;
+        }
+        #endregion
+
+        #region Write & Read
+        /// <summary>
+        /// if you can read return True
+        /// </summary>
+        /// <returns></returns>
+        public bool OnRead(int _readSize)
+        {
+            if(ReadSize < _readSize)
+                return false;
+            readPosition += _readSize;
+            return true;
+        }
+
+        /// <summary>
+        /// if you can write return True
+        /// </summary>
+        /// <returns></returns>
+        public bool OnWrite(int _writeSize)
+        {
+            if (WriteSize < _writeSize)
+                return false;
+            writePosition += _writeSize;    
+            return false;
+        }
+        #endregion
     }
 }
